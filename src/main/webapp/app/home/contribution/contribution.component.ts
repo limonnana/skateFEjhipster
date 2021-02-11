1c14bd575e70117d1537e9164a3a6917dc2cc467
limonnana <rosenzvaig@gmail.com>
2021-02-11 17:16:40 +0200

    autocomplate

--- src/main/webapp/app/home/contribution/contribution.component.ts
+++ src/main/webapp/app/home/contribution/contribution.component.ts
@@ -2,8 +2,11 @@ import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { EventService } from 'app/entities/event/event.service';
+import { FanService } from 'app/entities/fan/fan.service';
 import { IEvent } from 'app/shared/model/event.model';
+import { Fan, IFan } from 'app/shared/model/fan.model';
 import { ITrick } from 'app/shared/model/trick.model';
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead/typeahead-match.class';
 
 @Component({
   selector: 'jhi-contribution',
@@ -13,15 +16,71 @@ import { ITrick } from 'app/shared/model/trick.model';
 export class ContributionComponent implements OnInit {
   event?: IEvent | undefined;
   tricks?: ITrick[];
+  fans: IFan[] = [];
   isSaving = false;
+  selectedOption: any;
+
+  states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+    'Arkansas',
+    'California',
+    'Colorado',
+    'Connecticut',
+    'Delaware',
+    'Florida',
+    'Georgia',
+    'Hawaii',
+    'Idaho',
+    'Illinois',
+    'Indiana',
+    'Iowa',
+    'Kansas',
+    'Kentucky',
+    'Louisiana',
+    'Maine',
+    'Maryland',
+    'Massachusetts',
+    'Michigan',
+    'Minnesota',
+    'Mississippi',
+    'Missouri',
+    'Montana',
+    'Nebraska',
+    'Nevada',
+    'New Hampshire',
+    'New Jersey',
+    'New Mexico',
+    'New York',
+    'North Dakota',
+    'North Carolina',
+    'Ohio',
+    'Oklahoma',
+    'Oregon',
+    'Pennsylvania',
+    'Rhode Island',
+    'South Carolina',
+    'South Dakota',
+    'Tennessee',
+    'Texas',
+    'Utah',
+    'Vermont',
+    'Virginia',
+    'Washington',
+    'West Virginia',
+    'Wisconsin',
+    'Wyoming',
+  ];
 
   contributionForm = this.fb.group({
     amount: [],
     fan: [],
     trick: [],
+    state: [],
   });
 
-  constructor(private fb: FormBuilder, private eventService: EventService) {}
+  constructor(private fb: FormBuilder, private eventService: EventService, private fanService: FanService) {}
 
   ngOnInit(): void {
     this.eventService.getActive().subscribe((res: HttpResponse<IEvent>) => {
@@ -29,6 +88,7 @@ export class ContributionComponent implements OnInit {
       this.tricks = this.event?.tricks;
       this.updateForm(this.event!);
     });
+    this.loadAll();
   }
 
   updateForm(event: IEvent): void {
@@ -36,6 +96,16 @@ export class ContributionComponent implements OnInit {
       tricks: event.tricks,
     });
   }
+
+  loadAll(): void {
+    this.fanService.query().subscribe((res: HttpResponse<IFan[]>) => (this.fans = res.body || []));
+  }
+
+  onSelect(event: TypeaheadMatch): void {
+    this.selectedOption = event.item;
+    alert(this.selectedOption.id + ' ' + this.selectedOption.fullName);
+  }
+
   /*
 
   private createFromForm(): IEvent {
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -5,9 +5,11 @@ import { JhipsterFrontEndSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
 import { ContributionComponent } from './contribution/contribution.component';
+import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [JhipsterFrontEndSharedModule, RouterModule.forChild(HOME_ROUTE)],
+  imports: [BrowserAnimationsModule, JhipsterFrontEndSharedModule, RouterModule.forChild(HOME_ROUTE), TypeaheadModule.forRoot()],
   declarations: [HomeComponent, ContributionComponent],
 })
 export class JhipsterFrontEndHomeModule {}
